23dc50a5f6f25685b06454d0d824478b27bf1a16
Vladimir Korolev <vovsnmail@gmail.com>
2025-07-06 23:41:51 +0300

    test data loader

--- tests/test_solution.py
+++ tests/test_solution.py
@@ -13,8 +13,7 @@ from main import (
 
 @pytest.fixture
 def sample_csv_text():
-    return """
-name,brand,price,rating
+    return """name,brand,price,rating
 iphone 15 pro,apple,999,4.9
 galaxy s23 ultra,samsung,1199,4.8
 redmi note 12,xiaomi,199,4.6
@@ -32,7 +31,7 @@ iphone 13 mini,apple,599,4.5
 def sample_csv_data(sample_csv_text):
     loader = CSVDataLoader()
     with patch('builtins.open', return_value=io.StringIO(sample_csv_text)):
-        return loader.load('dummy_patch.csv')
+        return loader.load('anyfile.csv')
     
 
 def test_is_numeric():
@@ -42,3 +41,19 @@ def test_is_numeric():
     assert is_numeric('abc') is False
     assert is_numeric(None) is False
     assert is_numeric("") is False
+
+
+def test_csv_data_loader_success(sample_csv_text):
+    loader = CSVDataLoader()
+    with patch('builtins.open', return_value=io.StringIO(sample_csv_text)):
+        data = loader.load('anyfile.csv')
+        assert len(data) == 10
+        assert data[0]['name'] == 'iphone 15 pro'
+        assert data[0]['price'] == '999'
+
+
+def test_csv_data_loader_empty_file():
+    loader = CSVDataLoader()
+    with patch('builtins.open', return_value=io.StringIO('')):
+        data = loader.load('anyfile.csv')
+        assert data == []
